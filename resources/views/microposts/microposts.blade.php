1336fec2c20c756425831494525f8758c2ce6efc
gaku.murakami <cm150205@g.hit-u.ac.jp>
2018-06-20 04:13:29 +0000

    finalize

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -5,21 +5,20 @@
         <div class="media-left">
             <img class="media-object img-rounded" src="{{ Gravatar::src($user->email, 50) }}" alt="">
         </div>
+        
         <div class="media-body">
+            
             <div>
                 {!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!} <span class="text-muted">posted at {{ $micropost->created_at }}</span>
             </div>
-            <div>
-                <p>{!! nl2br(e($micropost->content)) !!}</p>
-                @include('favorite.favorite_button', ['microposts' => $microposts])
-            </div>
-            <div>
-                @if (Auth::user()->id == $micropost->user_id)
-                    {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
-                        {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
-                    {!! Form::close() !!}
-                @endif
-            </div>
+            
+            <div><p>{!! nl2br(e($micropost->content)) !!}</p></div>
+            
+        <div class="row">
+            <div class = "col-md-1" style="margin-right:15px;">@include('favorite.favorite_button', ['micropost' => $micropost])</div>
+            <div class = "col-md-1">@include('microposts.microposts_delete_button', ['micropost' => $micropost])</div>
+        </div>
+            
         </div>
     </li>
 @endforeach
